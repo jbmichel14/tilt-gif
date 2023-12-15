c16e32a8e18fdad9137ecc890519c9589c7f4c70
jbmichel14 <jbmichel66@gmail.com>
2023-12-15 18:12:55 +0100

    ik4meragram

--- index.js
+++ index.js
@@ -1,56 +1,66 @@
 
-const files = ["assets/IMG_7392.jpg", "assets/IMG_7393.jpg", "assets/IMG_7394.jpg", "assets/IMG_7395.jpg"];
-const filesLength = files.length
-const range = 50
+// const files = ["assets/gif/IMG_7392.jpg", "assets/gif/IMG_7393.jpg", "assets/gif/IMG_7394.jpg", "assets/gif/IMG_7395.jpg"];
+// const filesLength = files.length
+// const range = 50
 
-function handleOrientation(e){
+// function handleOrientation(e){
 
-  const gamma = Math.min(Math.max(e.gamma + range/2, 0), range);
+//   const gamma = Math.min(Math.max(e.gamma + range/2, 0), range);
   
   
-  // console.log(gamma);
+//   // console.log(gamma);
 
-  const index = Math.min(Math.floor(gamma/(range/filesLength)), filesLength-1);
+//   const index = Math.min(Math.floor(gamma/(range/filesLength)), filesLength-1);
 
-  console.log(index);
+//   console.log(index);
 
-  document.getElementById("img").src = files[index];
+//   document.getElementById("gif").src = files[index];
 
-}
+// }
 
 async function requestDeviceOrientation() {
 
-  document.getElementById("information").innerHTML = "requesting";
+  console.log("requesting")
   if(typeof DeviceOrientationEvent != 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function'){
 
-    document.getElementById("information").innerHTML = "requesting, ios ?";
+    console.log("requesting ios ?");;
     try{
       const permissionState = await DeviceOrientationEvent.requestPermission()
-      document.getElementById("information").innerHTML = "requesting, ios 1 ?";
+      console.log("requesting ios 1");
       if(permissionState === 'granted'){
-        document.getElementById("information").innerHTML = "requesting, ios ok";
+        console.log("ios ok");
         window.addEventListener('deviceorientation', handleOrientation)
       }
     }catch(error){
       console.error(error)
-      document.getElementById("information").innerHTML = "requesting, ios :((";
+      console.log("requesting ios :(((");
     }
   }else if('DeviceOrientationEvent' in window){
     window.addEventListener('deviceorientation', handleOrientation)
-    document.getElementById("information").innerHTML = "requesting, easy";
+    console.log("requesting easy");
   }else{
     alert('not supported')
-    document.getElementById("information").innerHTML = "not supported";
+    console.log("not supported");
   }
 }
+function handleDoubleClick(event){
 
+  var section = event.currentTarget;
 
-let container = document.querySelector(".container");
+  if (!event.target.closest('.ig_post_content')) {
+    return
+  }
 
-let icon = document.querySelector("i");
+  var postAction = section.querySelector('.ig_post_action');
+  if (postAction) {
+    var heartIcon = postAction.querySelector('.fa-heart-o');
+    if (heartIcon) {
+      heartIcon.classList.remove('fa-heart-o');
+      heartIcon.classList.add('fa-heart');
+    }
+  }
 
-function handleDoubleClick(){
-  console.log("segesr")
+  var icon = section.querySelector("i")
   icon.style.transform = "translate(-50%,50%) scale(1)";
   icon.style.opacity = 0.8;
   icon.style.color = "red";
@@ -61,5 +71,17 @@ function handleDoubleClick(){
   setTimeout(function () {
     icon.style.transform = "translate(-50%,50%) scale(0)";
   }, 1000);
+
   
+}
+
+function like(icon) {
+  var liked = icon.classList.contains('fa-heart')
+  if (liked) {
+    icon.classList.remove('fa-heart');
+    icon.classList.add('fa-heart-o');
+  } else {
+    icon.classList.remove('fa-heart-o');
+    icon.classList.add('fa-heart');
+  }
 }
